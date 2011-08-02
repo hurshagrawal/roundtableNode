24e74c7291489c6bf3563d25293a7029330aa187
Hursh <hsa470@gmail.com>
2011-08-02 19:20:57 -0400

    bookmarklet creation

--- public/javascripts/rtbookmarklet.js
+++ public/javascripts/rtbookmarklet.js
@@ -194,6 +194,6 @@ function runthis() {
 	rt.iframe = $("#roundtable_div");
 	rt.iframe.hide();
 	rt.iframe.append(rt.str).fadeIn();
-	rt.iframe.draggable();
+	rt.iframe.draggable({iframeFix: true, opacity: 0.75);
 
 }
