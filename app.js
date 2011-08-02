723dc129247ec9f13aa1fb68db699eb9d429f517
Hursh <hsa470@gmail.com>
2011-08-01 22:21:34 -0400

    converted to coffeescript

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
+(function() {
+  var app, express, fugue, http, step, sys, url;
+  express = require('express');
+  sys = require('sys');
+  url = require('url');
+  http = require('http');
+  step = require('step');
+  fugue = require('fugue');
+  app = module.exports = express.createServer();
+  app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.session({
+      secret: 'paneldiscussions'
+    }));
+    app.use(app.router);
+    return app.use(express.static(__dirname + 'public'));
   });
-});
-
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  app.configure('development', function() {
+    return app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
+  });
+  app.configure('production', function() {
+    return app.use(express.errorHandler());
+  });
+  app.get('/', function(req, res) {
+    return res.render('index', {});
+  });
+  app.listen(3000);
+}).call(this);
