080f23fd373acb92f9941cb6c9f23d279da91039
Hursh <hsa470@gmail.com>
2011-08-03 05:19:11 -0400

    testing serverside response to bookmarklet

--- app.js
+++ app.js
@@ -249,7 +249,7 @@ app.get('/createRoundtable', function(req, res) {
 				
 				http.request(options, function(res) {
 					res.setEncoding('utf8');
-					res.on('data', group());
+					res.on('data', group(userInfo));
 				}).on('error', function(err) {
 					console.log(err);
 				}).end();	
